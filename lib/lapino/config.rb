7d8881a3d1ee37840fe80c8b52298d0b7a14c97b
azukiwasher <azukiwasher@higanworks.com>
2012-07-24 11:34:47 +0900

    First commit

--- lib/lapino/config.rb
+++ lib/lapino/config.rb
@@ -1,21 +1,37 @@
 # encoding: utf-8
+require 'uri'
 
 module Lapino
   class Config
     class << self
-      attr_accessor :user, :pass, :host, :port, :vhost
+      attr_accessor :amqp_url
       attr_accessor :exchange, :routing_key
       attr_accessor :logging
 
-      def options
+      def amqp_config
+        uri = URI.parse(amqp_url)
         {
-          user: @user,
-          pass: @pass,
-          host: @host,
-          port: @port,
-          vhost: @vhost,
-          logging: @logging
+          user: uri.user,
+          pass: uri.password,
+          host: uri.host,
+          port: (uri.port || 5672),
+          vhost: uri.path,
+          logging: @logging || false
         }
+      rescue Object => e
+        raise "Invalid AMQP url: #{uri.inspect} (#{e})"
+      end
+
+      def amqp_url
+        @amqp_url ||= ENV["AMQP_URL"] || "amqp://guest:guest@localhost"
+      end
+
+      def exchange
+        @exchange ||= "amq.direct"
+      end
+
+      def routing_key
+        @routing_key ||= "*"
       end
     end
   end
--- lib/lapino/server.rb
+++ lib/lapino/server.rb
@@ -1,20 +1,16 @@
 # encoding: utf-8
 
 require 'sinatra/base'
-require 'lapino/config'
 require 'multi_json'
 
 module Lapino
   class Server < Sinatra::Base
 
-    use Rack::Auth::Basic do |user, pass|
-      user == Lapino::Config.user && pass == Lapino::Config.pass
-    end
-
-    # Reduce the amount of data transferred from the app.
+    # Reduce the amount of data transferred 
+    # from the app.
     use Rack::Deflater
 
-    set :raise_error => false
+    set :raise_error    => false
     set :show_exception => false
 
     helpers do
@@ -22,7 +18,7 @@ module Lapino
       def json_halt(code, reaseon)
         content_type "application/json"
         halt code
-        MultiJson.encode({
+        ::MultiJson.encode({
           status: code,
           reason: reason
         })
@@ -30,7 +26,7 @@ module Lapino
 
       def validate_json(data)
         begin
-          decoded_data = MultiJson.decode(data)
+          ::MultiJson.decode(data)
           return true
         rescue
           return false
@@ -39,32 +35,27 @@ module Lapino
     end
 
     error do
-      #e = request.env['sinatra.error']
       e = request.env['sinatra.error']
-      puts "ERROR: #{e.class}: #{e.message}"
+      Lapino.log "ERROR: #{e.class}: #{e.message}"
       json_halt 412, e.message
     end
 
-    def client
-      unless @@client
-        c = Bunny.new(Lapino::Config.options)
-        c.start
-        @@client = c
-      end
-      @@client
-    end
-
+    # Create an exchange from the current AMQP connection.
     def exchange
-      @@exchange ||= client.exchange(Lapino::Config.exchange, type: 'direct')
+      @exchange ||= Lapino.client.exchange(Config.exchange, type: 'direct')
     end
 
+    # Recieve post data, validate and redirect it
+    # to an AMQP broker.
     post '/publish', provides: :json do
       content_type :json
       if validate_json(request.body)
-        halt 400
-      else
-        exchange.publish(request.body, key: Lapino::Config.routing_key)
+        exchange.publish(request.body, key: Config.routing_key)
+        # No Content
         status 204
+      else
+        # Bad request
+        halt 400
       end
     end
   end
