d876e0817e90d1c6332f2d0af8324ec0ad457da6
Azukiwasher <azukiwasher@higanworks.com>
2013-02-21 19:19:37 +0900

    Updated gemspec

---
+++ lapin.gemspec
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'lapin/version'
+
+Gem::Specification.new do |s|
+  s.name        = "lapin"
+  s.version     = Lapin::VERSION
+  s.authors     = ["azukiwasher"]
+  s.email       = ["azukiwasher@higanworks.com"]
+  s.homepage    = "https://github.com/giraffi/lapin.git"
+  s.summary     = %q{A sinatra-based app that redirects data received via HTTP to an AMQP message broker}
+  s.description = %q{A sinatra-based app that redirects data received via HTTP to an AMQP message broker.}
+
+  s.rubyforge_project = "lapin"
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  s.add_dependency "rack",        "~> 1.4.0"
+  s.add_dependency "bunny",       "~> 0.8.0"
+  s.add_dependency "multi_json",  "~> 1.3.0"
+  s.add_dependency "sinatra",     "~> 1.3.0"
+
+  s.add_development_dependency "rake"
+  s.add_development_dependency "yajl-ruby"
+  s.add_development_dependency "rack-test"
+  s.add_development_dependency "shoulda"
+  s.add_development_dependency "simplecov"
+  s.add_development_dependency "mocha"
+  s.add_development_dependency "thin"
+end
---
+++ lib/lapin/server.rb
@@ -0,0 +1,83 @@
+# encoding: utf-8
+require 'sinatra/base'
+require 'multi_json'
+
+module Lapin
+  class Server < Sinatra::Base
+
+    # Reduce the amount of data transferred 
+    # from the app.
+    use Rack::Deflater
+
+    set :raise_error    => false
+    set :show_exception => false
+
+    helpers do
+      # define helper methods
+      def json_status(code, reason)
+        content_type "application/json"
+        status code
+        MultiJson.encode({:status => code, :reason => reason})
+      end
+
+      def validate_json(data)
+        begin
+          if MultiJson.respond_to?(:adapter)
+            MultiJson.load(data)
+          else
+            MultiJson.decode(data)
+          end
+          return true
+        rescue
+          return false
+        end
+      end
+    end
+
+    error do
+      e = request.env['sinatra.error']
+      Lapin.log "Error: #{e.class}: #{e.message}"
+      json_status 412, e.message
+    end
+
+    # Create an exchange from the current AMQP connection.
+    def exchange
+      @exchange ||= Lapin.client.exchange(Config.exchange, :type => 'direct')
+    end
+
+    # Recieve post data, validate and redirect it
+    # to an AMQP broker.
+    post '/publish.json' do
+      content_type :json
+
+      begin
+        # Set routing key from headers
+        Config.routing_key = request.env['HTTP_X_ROUTING_KEY']
+
+        # Set payload from bod
+        payload = request.body.read
+
+        if validate_json(payload)
+          exchange.publish(payload, :key => Config.routing_key)
+
+          # No Content
+          status 204
+        else
+          # Bad request
+          status 400
+        end
+      rescue Errno::ECONNRESET => e
+        Lapin.log "Error: #{e.class}: #{e.message}"
+        Lapin.client = nil
+
+        # Bad gateway
+        json_status 502, e.message
+      rescue Bunny::ConnectionError, Bunny::ServerDownError => e
+        Lapin.log "Error: #{e.class}: #{e.message}"
+
+        # Bad gateway
+        json_status 502, e.message
+      end
+    end
+  end
+end
